333dc7d35b067569dea9db5d940706531cc9f422
Christian Liebel <christian.liebel@thinktecture.com>
2019-12-06 08:58:38 +0100

    Lab #60

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@angular/core';
 import { Todo } from './todo';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
-  public create(todo: Todo): Todo {
+  public create(todo: Todo): Observable<Todo> {
     return;
   }
 
-  public get(todoId: number): Todo {
+  public get(todoId: number): Observable<Todo> {
     return;
   }
 
-  public getAll(): Todo[] {
-    return [
+  public getAll(): Observable<Todo[]> {
+    return of([
       { name: 'Wäsche waschen', id: 1, done: false },
       { name: 'Straße fegen', id: 2, done: true },
-    ];
+    ]);
   }
 
-  public update(todo: Todo): void {}
+  public update(todo: Todo): Observable<void> {
+    return of();
+  }
 
-  public delete(todoId: number): void {}
+  public delete(todoId: number): Observable<void> {
+    return of();
+  }
 }
